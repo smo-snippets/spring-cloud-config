d388f3fc208d7987255e4199ceeb93312a4925fa
Nils Smolinski <nils.smolinski@lyth.de>
2021-01-14 13:51:17 +0100

    Microservices verschoben nach spring-cloud-microservices

--- currency-exchange-service/README.md
+++
@@ -1,3 +0,0 @@
-# H2 Database
-jdbc:h2:mem:testdb
-data.sql automatically executed by spring boot
--- currency-exchange-service/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO exchange_value(id, currency_from, currency_to, conversion_multiple)
-values(10001, 'USD', 'INR', 65);
-INSERT INTO exchange_value(id, currency_from, currency_to, conversion_multiple)
-values(10002, 'EUR', 'INR', 75);
-INSERT INTO exchange_value(id, currency_from, currency_to, conversion_multiple)
-values(10003, 'AUD', 'INR', 25);
